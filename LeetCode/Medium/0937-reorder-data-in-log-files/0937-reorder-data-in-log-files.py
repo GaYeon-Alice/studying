98023acfda21fdea1ed19154cafc0b88e0bf2727
GaYeon-Alice <146514128+GaYeon-Alice@users.noreply.github.com>
2024-01-17 21:39:43 +0900

    Delete LeetCode directory

--- LeetCode/Medium/0937-reorder-data-in-log-files/0937-reorder-data-in-log-files.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def reorderLogFiles(self, logs: List[str]) -> List[str]:
-        let, dig = [], []
-
-        for log in logs:
-            let.append(log) if log.split()[1].isalpha() else dig.append(log)
-        let.sort(key=lambda x: (x.split()[1:], x.split()[0]))
-            
-        return let + dig
