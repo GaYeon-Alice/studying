e059a7268914b6102ff9b97e693b71b106deff12
GaYeon-Alice <146514128+GaYeon-Alice@users.noreply.github.com>
2024-01-17 22:11:12 +0900

    Delete LeetCode/Easy/0125-valid-palindrome directory

--- LeetCode/Easy/0125-valid-palindrome/0125-valid-palindrome.py
+++
@@ -1,5 +0,0 @@
-class Solution:
-    def isPalindrome(self, s: str) -> bool:
-        s = re.sub('[^a-zA-Z0-9]', '', s).lower()
-        reversed_s = ''.join(reversed(s))
-        return s == reversed_s
