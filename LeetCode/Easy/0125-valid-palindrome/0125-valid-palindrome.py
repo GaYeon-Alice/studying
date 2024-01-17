ee5c9af9475f35a9d7d31e340412fc5ff8791bc6
GaYeon-Alice <146514128+GaYeon-Alice@users.noreply.github.com>
2024-01-17 22:10:30 +0900

    [Easy] Title: , Time: 45 ms (75.57%), Memory: 19.4 MB (14.53%) - LeetHub

---
+++ LeetCode/Easy/0125-valid-palindrome/0125-valid-palindrome.py
@@ -0,0 +1,5 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = re.sub('[^a-zA-Z0-9]', '', s).lower()
+        reversed_s = ''.join(reversed(s))
+        return s == reversed_s
