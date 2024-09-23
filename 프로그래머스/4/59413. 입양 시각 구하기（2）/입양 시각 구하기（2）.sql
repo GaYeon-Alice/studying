0d0e3ab89d9f6271a9e96244844e4aa3e36740ff
GaYeon-Alice <146514128+GaYeon-Alice@users.noreply.github.com>
2024-09-23 22:27:26 +0900

    [level 4] Title: 입양 시각 구하기(2), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/59413. 입양 시각 구하기（2）/입양 시각 구하기（2）.sql
+++ 프로그래머스/4/59413. 입양 시각 구하기（2）/입양 시각 구하기（2）.sql
@@ -4,10 +4,10 @@ SET @HOUR = -1;
 
 -- 각 시간대별 입양 건수 계산
 SELECT
-    (@HOUR := @HOUR + 1) AS HOUR,  -- 시간 값을 1씩 증가시켜 0시부터 23시까지 반복
-    (SELECT COUNT(HOUR(DATETIME))  -- 해당 시간대에 일어난 입양 건수 조회
-     FROM ANIMAL_OUTS
-     WHERE HOUR(DATETIME) = @HOUR) AS COUNT
+    (@HOUR := @HOUR + 1) AS HOUR  -- 시간 값을 1씩 증가시켜 0시부터 23시까지 반복
+    , (SELECT COUNT(HOUR(DATETIME))  -- 해당 시간대에 일어난 입양 건수 조회
+       FROM ANIMAL_OUTS
+       WHERE HOUR(DATETIME) = @HOUR) AS COUNT
 FROM
     ANIMAL_OUTS
 WHERE
