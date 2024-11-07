aa186811801f81889c524d22a9f53be757f96636
GaYeon-Alice <146514128+GaYeon-Alice@users.noreply.github.com>
2024-11-07 15:54:04 +0900

    Time: 307 ms (84.71%), Space: 0B (100%) - LeetHub

--- LeetCode/Medium/0602-friend-requests-ii-who-has-the-most-friends/0602-friend-requests-ii-who-has-the-most-friends.sql
+++ LeetCode/Medium/0602-friend-requests-ii-who-has-the-most-friends/0602-friend-requests-ii-who-has-the-most-friends.sql
@@ -1,13 +1,13 @@
 # Write your MySQL query statement below
-WITH Relation AS (
+WITH Relation AS (  -- 모든 사용자 간 양방향 관계
     SELECT requester_id
          , accepter_id
     FROM RequestAccepted
-    UNION ALL
+    UNION
     SELECT accepter_id
          , requester_id
     FROM RequestAccepted
-), FriendCounts AS (
+), FriendCounts AS (  -- 각 사용자별 친구 수
     SELECT requester_id
          , COUNT(accepter_id) AS num
     FROM Relation
