1ea4df95151af1b3978beb16d27c6291ce26a179
GaYeon-Alice <146514128+GaYeon-Alice@users.noreply.github.com>
2024-11-13 21:54:26 +0900

    Time: 666 ms (47.59%), Space: 0B (100%) - LeetHub

--- LeetCode/Medium/0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
+++ LeetCode/Medium/0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
@@ -6,6 +6,6 @@ WITH LoginHistory AS (
     GROUP BY player_id
 )
 -- 처음 로그인한 날부터 그다음 날까지 이틀 연속으로 로그인한 사용자만 조회
-SELECT ROUND(SUM(IF(lh.first_login + 1 = a.event_date, 1, 0)) / COUNT(DISTINCT a.player_id), 2) AS fraction  
+SELECT ROUND(SUM(IF(DATE_ADD(lh.first_login, INTERVAL 1 DAY) = a.event_date, 1, 0)) / COUNT(DISTINCT a.player_id), 2) AS fraction  
 FROM Activity AS a
 JOIN LoginHistory AS lh ON a.player_id = lh.player_id;
