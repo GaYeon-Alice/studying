876c4218d0b9ceb18fd5ff16b8437cd22b496e51
GaYeon-Alice <146514128+GaYeon-Alice@users.noreply.github.com>
2024-12-19 22:50:20 +0900

    [level 2] Title: NULL 처리하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/59410. NULL 처리하기/NULL 처리하기.sql
+++ 프로그래머스/2/59410. NULL 처리하기/NULL 처리하기.sql
@@ -1,10 +1,9 @@
 -- 코드를 입력하세요
-SELECT
-    ANIMAL_TYPE,
-    CASE
-        WHEN NAME IS NULL THEN 'No name'
-        ELSE NAME
-    END AS NAME,
-    SEX_UPON_INTAKE
+SELECT ANIMAL_TYPE
+     , CASE
+           WHEN NAME IS NULL THEN 'No name'  -- 이름이 없는 동물의 이름은 'No name'으로 표시
+           ELSE NAME
+       END AS NAME
+     , SEX_UPON_INTAKE
 FROM ANIMAL_INS
-ORDER BY ANIMAL_ID
+ORDER BY ANIMAL_ID;
