feec886ab62622d6968ddf40f6ad1b530eefcc31
GaYeon-Alice <146514128+GaYeon-Alice@users.noreply.github.com>
2024-12-24 23:57:33 +0900

    [level 3] Title: 오랜 기간 보호한 동물(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/README.md
+++ 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/README.md
@@ -1,6 +1,6 @@
 # [level 3] 오랜 기간 보호한 동물(1) - 59044 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/59044#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/59044) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@ Empty
 
 ### 제출 일자
 
-2024년 09월 18일 22:57:22
+2024년 12월 24일 23:57:30
 
 ### 문제 설명
 
--- 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
+++ 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
@@ -1,14 +1,8 @@
 -- 코드를 입력하세요
-SELECT
-    I.NAME,
-    I.DATETIME
-FROM
-    ANIMAL_INS AS I
-LEFT JOIN
-    ANIMAL_OUTS AS O
-    ON I.ANIMAL_ID = O.ANIMAL_ID
-WHERE
-    O.ANIMAL_ID IS NULL  -- 아직 입양을 못 간 동물 필터링
-ORDER BY
-    I.DATETIME  -- 보호소에 들어온 순서대로 정렬
+SELECT I.NAME
+     , I.DATETIME
+FROM ANIMAL_INS AS I
+LEFT JOIN ANIMAL_OUTS AS O ON I.ANIMAL_ID = O.ANIMAL_ID
+WHERE O.DATETIME IS NULL  -- 아직 입양을 못 간 동물 필터링
+ORDER BY I.DATETIME
 LIMIT 3;  -- 가장 오래 보호소에 있었던 동물 3마리 조회
