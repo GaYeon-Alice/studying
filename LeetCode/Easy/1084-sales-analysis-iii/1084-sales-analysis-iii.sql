3d0848eb8f9c619ee0c55c0f69da90dd110fa8d2
GaYeon-Alice <146514128+GaYeon-Alice@users.noreply.github.com>
2025-01-04 19:31:17 +0900

    Time: 2330 ms (10.43%), Space: 0B (100%) - LeetHub

--- LeetCode/Easy/1084-sales-analysis-iii/1084-sales-analysis-iii.sql
+++ LeetCode/Easy/1084-sales-analysis-iii/1084-sales-analysis-iii.sql
@@ -1,13 +1,7 @@
-# Write your MySQL query statement below
-SELECT DISTINCT product_id
-              , product_name
-FROM Product
-WHERE product_id IN (
-    SELECT product_id
-    FROM Sales
-    WHERE sale_date BETWEEN '2019-01-01' AND '2019-03-31'
-) AND product_id NOT IN (
-    SELECT product_id
-    FROM Sales
-    WHERE sale_date BETWEEN '2019-04-01' AND '2019-12-31'
-);
+SELECT p.product_id
+     , p.product_name
+FROM Product AS p
+JOIN Sales AS s ON p.product_id = s.product_id
+GROUP BY s.product_id
+HAVING MIN(s.sale_date) >= '2019-01-01'
+  AND MAX(s.sale_date) <= '2019-03-31';
