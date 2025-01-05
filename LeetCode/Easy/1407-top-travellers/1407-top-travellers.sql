8df78b9b736c4f01604b74e038452690e89056b7
GaYeon-Alice <146514128+GaYeon-Alice@users.noreply.github.com>
2025-01-05 14:43:22 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ LeetCode/Easy/1407-top-travellers/1407-top-travellers.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+SELECT u.name
+     , SUM(CASE WHEN r.distance IS NULL THEN 0 ELSE r.distance END) AS travelled_distance
+FROM Users AS u
+LEFT JOIN Rides AS r ON u.id = r.user_id
+GROUP BY u.name
+ORDER BY travelled_distance DESC
+       , u.name;
