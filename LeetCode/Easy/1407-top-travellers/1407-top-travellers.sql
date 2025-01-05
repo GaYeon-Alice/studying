e2b8cf4448da1269ee5faf0cf3a867033f8ed29a
GaYeon-Alice <146514128+GaYeon-Alice@users.noreply.github.com>
2025-01-05 14:53:00 +0900

    Time: 1309 ms (30.16%), Space: 0B (100%) - LeetHub

--- LeetCode/Easy/1407-top-travellers/1407-top-travellers.sql
+++ LeetCode/Easy/1407-top-travellers/1407-top-travellers.sql
@@ -3,6 +3,6 @@ SELECT u.name
      , SUM(CASE WHEN r.distance IS NULL THEN 0 ELSE r.distance END) AS travelled_distance
 FROM Users AS u
 LEFT JOIN Rides AS r ON u.id = r.user_id
-GROUP BY u.name
+GROUP BY u.id
 ORDER BY travelled_distance DESC
        , u.name;
