e1522856136d6f46700a2774bf32ea927c77d7d2
GaYeon-Alice <146514128+GaYeon-Alice@users.noreply.github.com>
2025-01-13 15:40:45 +0900

    [level 1] Title: Python 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/1/276013. Python 개발자 찾기/Python 개발자 찾기.sql
+++ 프로그래머스/1/276013. Python 개발자 찾기/Python 개발자 찾기.sql
@@ -1,7 +1,8 @@
 -- 코드를 작성해주세요
-SELECT ID, EMAIL, FIRST_NAME, LAST_NAME
-FROM DEVELOPER_INFOS
-WHERE SKILL_1 = 'Python'
-OR SKILL_2 = 'Python'
-OR SKILL_3 = 'Python'
-ORDER BY ID
+SELECT id
+     , email
+     , first_name
+     , last_name
+FROM developer_infos
+WHERE 'Python' IN (skill_1, skill_2, skill_3)
+ORDER BY id;
