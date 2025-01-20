08d7ebdce24b351f9c1430227f80d59de0b98673
GaYeon-Alice <146514128+GaYeon-Alice@users.noreply.github.com>
2025-01-20 16:28:58 +0900

    Time: 260 ms (82.78%), Space: 0B (100%) - LeetHub

--- LeetCode/Medium/1934-confirmation-rate/1934-confirmation-rate.sql
+++ LeetCode/Medium/1934-confirmation-rate/1934-confirmation-rate.sql
@@ -1,9 +1,6 @@
-# Write your MySQL query statement below
+-- Write your PostgreSQL query statement below
 SELECT s.user_id
-     , CASE
-           WHEN c.action IS NOT NULL THEN ROUND(SUM(IF(c.action = 'confirmed', 1, 0)) / COUNT(c.time_stamp), 2)
-           ELSE 0
-       END AS confirmation_rate
-FROM Signups AS s
-LEFT JOIN Confirmations AS c ON s.user_id = c.user_id
+     , ROUND(AVG(CASE WHEN c.action = 'confirmed' THEN 1.0 ELSE 0.0 END), 2) AS confirmation_rate
+FROM signups AS s
+LEFT JOIN confirmations AS c USING (user_id)
 GROUP BY s.user_id;
