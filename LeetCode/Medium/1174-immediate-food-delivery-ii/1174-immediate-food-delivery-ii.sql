3a358e9db1fd00eee06864a73efc3b4f0e90c678
GaYeon-Alice <146514128+GaYeon-Alice@users.noreply.github.com>
2025-01-20 16:47:13 +0900

    Time: 712 ms (40.49%), Space: 0B (100%) - LeetHub

--- LeetCode/Medium/1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
+++ LeetCode/Medium/1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
@@ -1,9 +1,9 @@
-# Write your MySQL query statement below
-SELECT ROUND(SUM(IF(order_date = customer_pref_delivery_date, 1, 0)) / COUNT(*) * 100, 2) AS immediate_percentage
-FROM Delivery
+-- Write your PostgreSQL query statement below
+SELECT ROUND(AVG(CASE WHEN order_date = customer_pref_delivery_date THEN 1.0 ELSE 0.0 END) * 100, 2) AS immediate_percentage
+FROM delivery
 WHERE (customer_id, order_date) IN (
     SELECT customer_id
          , MIN(order_date)
-    FROM Delivery
+    FROM delivery
     GROUP BY customer_id
 );
