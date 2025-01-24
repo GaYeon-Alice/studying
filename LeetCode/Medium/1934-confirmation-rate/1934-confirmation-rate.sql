9250c7fc945e758c41d330cec904a258c0032066
GaYeon-Alice <146514128+GaYeon-Alice@users.noreply.github.com>
2025-01-24 17:51:38 +0900

    Time: 492 ms (32.03%), Space: 0B (100%) - LeetHub

--- LeetCode/Medium/1934-confirmation-rate/1934-confirmation-rate.sql
+++ LeetCode/Medium/1934-confirmation-rate/1934-confirmation-rate.sql
@@ -1,6 +1,6 @@
 -- Write your PostgreSQL query statement below
 SELECT s.user_id
-     , ROUND(AVG(CASE WHEN c.action = 'confirmed' THEN 1.0 ELSE 0.0 END), 2) AS confirmation_rate
+     , ROUND(COUNT(*) FILTER (WHERE c.action = 'confirmed') / COUNT(*)::decimal, 2) AS confirmation_rate
 FROM signups AS s
 LEFT JOIN confirmations AS c USING (user_id)
 GROUP BY s.user_id;
