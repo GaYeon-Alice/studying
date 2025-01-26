a8b167dd9fd0fa8a76e5f6215cdfca358ed23501
GaYeon-Alice <146514128+GaYeon-Alice@users.noreply.github.com>
2025-01-26 16:46:36 +0900

    [level 2] Title: 분기별 분화된 대장균의 개체 수 구하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/299308. 분기별 분화된 대장균의 개체 수 구하기/분기별 분화된 대장균의 개체 수 구하기.sql
+++ 프로그래머스/2/299308. 분기별 분화된 대장균의 개체 수 구하기/분기별 분화된 대장균의 개체 수 구하기.sql
@@ -1,15 +1,11 @@
 -- 코드를 작성해주세요
-SELECT
-    CASE
-        WHEN MONTH(DIFFERENTIATION_DATE) IN ('1', '2', '3') THEN '1Q'
-        WHEN MONTH(DIFFERENTIATION_DATE) IN ('4', '5', '6') THEN '2Q'
-        WHEN MONTH(DIFFERENTIATION_DATE) IN ('7', '8', '9') THEN '3Q'
-        ELSE '4Q'
-    END AS QUARTER
-    , COUNT(*) AS ECOLI_COUNT
-FROM
-    ECOLI_DATA
-GROUP BY
-    QUARTER
-ORDER BY
-    QUARTER;
+SELECT CASE
+            WHEN MONTH(differentiation_date) IN (1, 2, 3) THEN '1Q'
+            WHEN MONTH(differentiation_date) IN (4, 5, 6) THEN '2Q'
+            WHEN MONTH(differentiation_date) IN (7, 8, 9) THEN '3Q'
+            ELSE '4Q'
+       END AS quarter
+     , COUNT(*) AS ecoli_count
+FROM ecoli_data
+GROUP BY quarter
+ORDER BY quarter;
