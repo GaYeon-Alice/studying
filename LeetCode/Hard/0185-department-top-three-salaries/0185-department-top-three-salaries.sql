efe567a2dabbc9e1743b15c10305663b10514132
GaYeon-Alice <146514128+GaYeon-Alice@users.noreply.github.com>
2025-01-27 17:08:48 +0900

    Time: 409 ms (58.93%), Space: 0B (100%) - LeetHub

--- LeetCode/Hard/0185-department-top-three-salaries/0185-department-top-three-salaries.sql
+++ LeetCode/Hard/0185-department-top-three-salaries/0185-department-top-three-salaries.sql
@@ -1,14 +1,14 @@
 -- Write your PostgreSQL query statement below
-WITH salary_rank AS (
+WITH SalaryRank AS (
     SELECT name
          , salary
          , departmentId
          , DENSE_RANK() OVER (PARTITION BY departmentId ORDER BY salary DESC) AS dr
-    FROM employee
+    FROM Employee
 )
 SELECT d.name AS "Department"
      , r.name AS "Employee"
      , r.salary AS "Salary"
-FROM salary_rank AS r
-     JOIN department AS d ON r.departmentId = d.id
+FROM SalaryRank AS r
+     LEFT JOIN Department AS d ON r.departmentId = d.id
 WHERE r.dr <= 3 
