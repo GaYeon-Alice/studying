fe8fb99bb1c3f6c56d1fb00c706504eb2de9bdf5
GaYeon-Alice <146514128+GaYeon-Alice@users.noreply.github.com>
2025-01-28 17:19:11 +0900

    [level 4] Title: 특정 세대의 대장균 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/301650. 특정 세대의 대장균 찾기/특정 세대의 대장균 찾기.sql
+++ 프로그래머스/4/301650. 특정 세대의 대장균 찾기/특정 세대의 대장균 찾기.sql
@@ -1,8 +1,7 @@
 -- 코드를 작성해주세요
-SELECT C2.ID
-FROM ECOLI_DATA AS P  -- P.PARENT_ID가 NULL이면 P.ID가 1세대 대장균임을 의미
-LEFT JOIN ECOLI_DATA AS C1 ON P.ID = C1.PARENT_ID  -- P.PARENT_ID가 NULL이면 C1.ID가 2세대 대장균임을 의미
-LEFT JOIN ECOLI_DATA AS C2 ON C1.ID = C2.PARENT_ID  -- P.PARENT_ID가 NULL이고 C2.ID가 존재하면 C2.ID가 3세대 대장균임을 의미
-WHERE P.PARENT_ID IS NULL
-  AND C2.ID IS NOT NULL
-ORDER BY C2.ID;
+SELECT c2.id
+FROM ecoli_data AS p
+     JOIN ecoli_data AS c ON p.id = c.parent_id
+     JOIN ecoli_data AS c2 ON c.id = c2.parent_id
+WHERE p.parent_id IS NULL
+ORDER BY c2.id;
