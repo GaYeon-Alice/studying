7c76b9bc1920c0dbf1a4f095ba769e2d5fa22af5
GaYeon-Alice <146514128+GaYeon-Alice@users.noreply.github.com>
2025-01-31 19:37:02 +0900

    Time: 634 ms (53.89%), Space: 0B (100%) - LeetHub

--- LeetCode/Easy/1148-article-views-i/1148-article-views-i.sql
+++ LeetCode/Easy/1148-article-views-i/1148-article-views-i.sql
@@ -1,5 +1,5 @@
-# Write your MySQL query statement below
+-- Write your PostgreSQL query statement below
 SELECT DISTINCT author_id AS id
 FROM Views
 WHERE author_id = viewer_id
-ORDER BY author_id;
+ORDER BY id
