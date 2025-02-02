4a89cc4e98739a2340cd532780ea8ddb62b2b9d3
GaYeon-Alice <146514128+GaYeon-Alice@users.noreply.github.com>
2025-02-02 17:52:17 +0900

    Time: 363 ms (27.85%), Space: 0B (100%) - LeetHub

--- LeetCode/Easy/1661-average-time-of-process-per-machine/1661-average-time-of-process-per-machine.sql
+++ LeetCode/Easy/1661-average-time-of-process-per-machine/1661-average-time-of-process-per-machine.sql
@@ -1,6 +1,5 @@
-# Write your MySQL query statement below
+-- Write your PostgreSQL query statement below
 SELECT machine_id
-     , ROUND(SUM(IF(activity_type = 'start', -timestamp, timestamp))  -- 'start' 타임스탬프를 음수로 변환하여 'end' 타임스탬프와의 차이 계산
-             / COUNT(DISTINCT process_id), 3) AS processing_time
+     , ROUND(SUM(CASE WHEN activity_type = 'start' THEN -timestamp ELSE timestamp END)::numeric / COUNT(DISTINCT process_id), 3) AS processing_time
 FROM Activity
-GROUP BY machine_id;
+GROUP BY machine_id
