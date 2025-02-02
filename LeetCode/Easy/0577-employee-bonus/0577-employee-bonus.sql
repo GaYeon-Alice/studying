d00f93114714f2831662880c840526707ac978fe
GaYeon-Alice <146514128+GaYeon-Alice@users.noreply.github.com>
2025-02-02 17:59:14 +0900

    Time: 551 ms (53.34%), Space: 0B (100%) - LeetHub

--- LeetCode/Easy/0577-employee-bonus/0577-employee-bonus.sql
+++ LeetCode/Easy/0577-employee-bonus/0577-employee-bonus.sql
@@ -1,7 +1,7 @@
-# Write your MySQL query statement below
+-- Write your PostgreSQL query statement below
 SELECT e.name
      , b.bonus
 FROM Employee AS e
-LEFT JOIN Bonus AS b ON e.empId = b.empId
+     LEFT JOIN Bonus AS b USING (empId)
 WHERE b.bonus < 1000
    OR b.bonus IS NULL;
