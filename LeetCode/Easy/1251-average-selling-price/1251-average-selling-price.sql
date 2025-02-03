41cf8db047857773e70c86009b9405b00529d60e
GaYeon-Alice <146514128+GaYeon-Alice@users.noreply.github.com>
2025-02-03 16:24:29 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Easy/1251-average-selling-price/1251-average-selling-price.sql
+++ LeetCode/Easy/1251-average-selling-price/1251-average-selling-price.sql
@@ -1,11 +1,8 @@
-# Write your MySQL query statement below
+-- Write your PostgreSQL query statement below
 SELECT p.product_id
-     , CASE
-         WHEN us.units IS NULL THEN 0
-         ELSE ROUND((SUM(p.price * us.units) / SUM(us.units)), 2)
-       END AS average_price
+     , ROUND(SUM(p.price * u.units) * 1.0 / SUM(u.units), 2) AS average_price
 FROM Prices AS p
-LEFT JOIN UnitsSold AS us ON p.product_id = us.product_id
-WHERE us.product_id IS NULL
-   OR us.purchase_date BETWEEN p.start_date AND p.end_date
+LEFT JOIN UnitsSold AS u
+    ON p.product_id = u.product_id
+    AND u.purchase_date BETWEEN p.start_date AND p.end_date
 GROUP BY p.product_id;
