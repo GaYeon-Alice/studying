343cb58a117c22394bbcba9886f56b421283f6bf
GaYeon-Alice <146514128+GaYeon-Alice@users.noreply.github.com>
2025-02-03 16:25:00 +0900

    Time: 200 ms (95.16%), Space: 0B (100%) - LeetHub

--- LeetCode/Easy/1251-average-selling-price/1251-average-selling-price.sql
+++ LeetCode/Easy/1251-average-selling-price/1251-average-selling-price.sql
@@ -1,6 +1,6 @@
 -- Write your PostgreSQL query statement below
 SELECT p.product_id
-     , ROUND(SUM(p.price * u.units) * 1.0 / SUM(u.units), 2) AS average_price
+     , COALESCE(ROUND(SUM(p.price * u.units) * 1.0 / SUM(u.units), 2), 0) AS average_price
 FROM Prices AS p
 LEFT JOIN UnitsSold AS u
     ON p.product_id = u.product_id
