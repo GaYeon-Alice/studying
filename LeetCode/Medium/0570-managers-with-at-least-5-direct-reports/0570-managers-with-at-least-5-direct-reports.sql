7f4437f0beba7179e74cbdc338a0f7d9928a5535
GaYeon-Alice <146514128+GaYeon-Alice@users.noreply.github.com>
2025-02-03 15:47:24 +0900

    Time: 469 ms (77.33%), Space: 0B (100%) - LeetHub

--- LeetCode/Medium/0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
+++ LeetCode/Medium/0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -1,6 +1,6 @@
 # Write your MySQL query statement below
 SELECT e.name
 FROM Employee AS m
-LEFT JOIN Employee AS e ON m.managerId = e.id
+JOIN Employee AS e ON m.managerId = e.id
 GROUP BY m.managerId
 HAVING COUNT(*) >= 5;  -- 5명 이상의 직원을 관리하는 관리자만 조회
