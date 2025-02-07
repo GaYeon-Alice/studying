42dac8a6296225d979a9978af977746d62b559bd
GaYeon-Alice <146514128+GaYeon-Alice@users.noreply.github.com>
2025-02-07 23:50:10 +0900

    Time: 686 ms (74.3%), Space: 0B (100%) - LeetHub

--- LeetCode/Medium/1193-monthly-transactions-i/1193-monthly-transactions-i.sql
+++ LeetCode/Medium/1193-monthly-transactions-i/1193-monthly-transactions-i.sql
@@ -4,6 +4,6 @@ SELECT TO_CHAR(trans_date, 'YYYY-MM') AS month
      , COUNT(*) AS trans_count
      , COUNT(*) FILTER (WHERE state = 'approved') AS approved_count
      , SUM(amount) AS trans_total_amount
-     , SUM(amount) FILTER (WHERE state = 'approved') AS approved_total_amount
+     , COALESCE(SUM(amount) FILTER (WHERE state = 'approved'), 0) AS approved_total_amount
 FROM Transactions
 GROUP BY month, country;
