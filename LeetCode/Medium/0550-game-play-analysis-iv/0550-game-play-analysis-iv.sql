08d8f6e9d985ccb094113dc160d4cea875aff4d4
GaYeon-Alice <146514128+GaYeon-Alice@users.noreply.github.com>
2025-02-09 15:46:40 +0900

    Time: 1741 ms (9.8%), Space: 0B (100%) - LeetHub

--- LeetCode/Medium/0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
+++ LeetCode/Medium/0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
@@ -1,7 +1,7 @@
-# Write your MySQL query statement below
-SELECT ROUND(COUNT(player_id) / (SELECT COUNT(DISTINCT player_id) FROM Activity), 2) AS fraction
+-- Write your PostgreSQL query statement below
+SELECT ROUND(COUNT(player_id) * 1.0 / (SELECT COUNT(DISTINCT player_id) FROM Activity), 2) AS fraction
 FROM Activity
-WHERE (player_id, DATE_SUB(event_date, INTERVAL 1 DAY)) IN (
+WHERE (player_id, event_date - 1) IN (
     SELECT player_id
          , MIN(event_date)
     FROM Activity
