46dc7602495cb5872a5b50ed96efbfee3d5fa31f
GaYeon-Alice <146514128+GaYeon-Alice@users.noreply.github.com>
2025-02-10 18:00:57 +0900

    Time: 1604 ms (11.26%), Space: 0B (100%) - LeetHub

--- LeetCode/Medium/1045-customers-who-bought-all-products/1045-customers-who-bought-all-products.sql
+++ LeetCode/Medium/1045-customers-who-bought-all-products/1045-customers-who-bought-all-products.sql
@@ -1,6 +1,5 @@
-# Write your MySQL query statement below
-SELECT c.customer_id
-FROM Customer AS c
-LEFT JOIN Product AS p ON c.product_key = p.product_key
-GROUP BY c.customer_id
-HAVING COUNT(DISTINCT c.product_key) = (SELECT COUNT(*) FROM Product)  -- 모든 제품을 구매한 고객만 선택;
+-- Write your PostgreSQL query statement below
+SELECT customer_id
+FROM Customer
+GROUP BY customer_id
+HAVING COUNT(DISTINCT product_key) = (SELECT COUNT(*) FROM Product)
