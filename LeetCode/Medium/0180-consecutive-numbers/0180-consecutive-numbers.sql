3abb1e5e1596a092886dbcc37814b24d515c4491
GaYeon-Alice <146514128+GaYeon-Alice@users.noreply.github.com>
2025-02-11 23:17:05 +0900

    Time: 393 ms (83.13%), Space: 0B (100%) - LeetHub

--- LeetCode/Medium/0180-consecutive-numbers/0180-consecutive-numbers.sql
+++ LeetCode/Medium/0180-consecutive-numbers/0180-consecutive-numbers.sql
@@ -1,11 +1,11 @@
-# Write your MySQL query statement below
-WITH Nums AS (
-    SELECT *
-         , LAG(num) OVER(ORDER BY id) AS prev_num
-         , LEAD(num) OVER(ORDER BY id) AS next_num
+-- Write your PostgreSQL query statement below
+WITH Numbers AS (
+    SELECT num
+         , LAG(num) OVER (ORDER BY id) AS prev_num
+         , LEAD(num) OVER (ORDER BY id) AS next_num
     FROM Logs
 )
 SELECT DISTINCT num AS ConsecutiveNums
-FROM Nums
-WHERE prev_num = num
-  AND num = next_num;
+FROM Numbers
+WHERE num = prev_num
+  AND num = next_num
