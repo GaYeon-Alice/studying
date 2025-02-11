470b775d334ce3318e07755d9f339da65fbcc9b3
GaYeon-Alice <146514128+GaYeon-Alice@users.noreply.github.com>
2025-02-11 23:57:37 +0900

    Time: 693 ms (51.5%), Space: 0B (100%) - LeetHub

--- LeetCode/Medium/1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
+++ LeetCode/Medium/1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
@@ -1,11 +1,11 @@
-# Write your MySQL query statement below
-WITH LatestChanges AS (
-    SELECT product_id
-         , MAX(CASE WHEN change_date <= '2019-08-16' THEN change_date END) AS change_date  -- 제품별로 가장 최근에 가격이 변동된 날짜 조회
+-- Write your PostgreSQL query statement below
+WITH LatestChange AS (
+    SELECT DISTINCT product_id
+                  , FIRST_VALUE (new_price) OVER (PARTITION BY product_id ORDER BY change_date DESC) AS price
     FROM Products
-    GROUP BY product_id
+    WHERE change_date <= '2019-08-16'
 )
-SELECT lc.product_id
-     , COALESCE(p.new_price, 10) AS price  -- 가격 변동이 있었다면 변동된 가격을, 없었다면 10을 출력
-FROM LatestChanges AS lc
-LEFT JOIN Products AS p ON lc.product_id = p.product_id AND p.change_date = lc.change_date;
+SELECT DISTINCT product_id
+              , COALESCE(price, 10) AS price
+FROM Products AS p
+     LEFT JOIN LatestChange AS l USING (product_id)
