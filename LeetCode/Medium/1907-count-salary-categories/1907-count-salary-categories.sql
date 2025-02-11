9105f0d920a2f006b088696b70c24027e6781819
GaYeon-Alice <146514128+GaYeon-Alice@users.noreply.github.com>
2025-02-12 00:13:47 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Medium/1907-count-salary-categories/1907-count-salary-categories.sql
+++ LeetCode/Medium/1907-count-salary-categories/1907-count-salary-categories.sql
@@ -1,12 +1,12 @@
-# Write your MySQL query statement below
+-- Write your PostgreSQL query statement below
 SELECT 'Low Salary' AS category
-     , SUM(IF(income < 20000, 1, 0)) AS accounts_count
+     , COUNT(*) FILTER (WHERE income < 20000) AS accounts_count
 FROM Accounts
-UNION
+UNION ALL
 SELECT 'Average Salary' AS category
-     , SUM(IF(income BETWEEN 20000 AND 50000, 1, 0)) AS accounts_count
+     , COUNT(*) FILTER (WHERE income BETWEEN 20000 AND 50000) AS accounts_count
 FROM Accounts
-UNION
+UNION ALL
 SELECT 'High Salary' AS category
-     , SUM(IF(income > 50000, 1, 0)) AS accounts_count
-FROM Accounts;
+     , COUNT(*) FILTER (WHERE income > 50000) AS accounts_count
+FROM Accounts
