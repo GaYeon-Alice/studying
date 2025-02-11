16a9c3d8bd9558a9fc44efe056e5f73380dfafe6
GaYeon-Alice <146514128+GaYeon-Alice@users.noreply.github.com>
2025-02-11 23:01:37 +0900

    Time: 636 ms (16.51%), Space: 0B (100%) - LeetHub

--- LeetCode/Easy/1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
+++ LeetCode/Easy/1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
@@ -3,8 +3,9 @@ SELECT employee_id
      , department_id
 FROM Employee
 WHERE primary_flag = 'Y'
-   OR employee_id NOT IN ( 
+   OR employee_id IN (
     SELECT employee_id
     FROM Employee
-    WHERE primary_flag = 'Y'
+    GROUP BY employee_id
+    HAVING COUNT(*) = 1
 )
