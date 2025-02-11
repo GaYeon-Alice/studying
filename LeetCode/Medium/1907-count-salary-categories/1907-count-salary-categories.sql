cfa47eb69bf3d9a3b1b37a186a9600c4cb64b185
GaYeon-Alice <146514128+GaYeon-Alice@users.noreply.github.com>
2025-02-12 00:06:00 +0900

    Time: 1828 ms (88.73%), Space: 0B (100%) - LeetHub

--- LeetCode/Medium/1907-count-salary-categories/1907-count-salary-categories.sql
+++ LeetCode/Medium/1907-count-salary-categories/1907-count-salary-categories.sql
@@ -1,22 +1,12 @@
--- Write your PostgreSQL query statement below
-WITH c AS (
-    SELECT 'Low Salary' AS category
-    UNION
-    SELECT 'Average Salary' AS category
-    UNION
-    SELECT 'High Salary' AS category
-)
-SELECT 
-    c.category,
-    COALESCE(accounts_count, 0) AS accounts_count
-FROM c
-LEFT JOIN (
-    SELECT CASE
-                WHEN income < 20000 THEN 'Low Salary'
-                WHEN income BETWEEN 20000 AND 50000 THEN 'Average Salary'
-                WHEN income > 50000 THEN 'High Salary'
-           END AS category
-         , COUNT(*) AS accounts_count
-    FROM Accounts
-    GROUP BY category
-) AS cnt ON c.category = cnt.category
+# Write your MySQL query statement below
+SELECT 'Low Salary' AS category
+     , SUM(IF(income < 20000, 1, 0)) AS accounts_count
+FROM Accounts
+UNION
+SELECT 'Average Salary' AS category
+     , SUM(IF(income BETWEEN 20000 AND 50000, 1, 0)) AS accounts_count
+FROM Accounts
+UNION
+SELECT 'High Salary' AS category
+     , SUM(IF(income > 50000, 1, 0)) AS accounts_count
+FROM Accounts;
