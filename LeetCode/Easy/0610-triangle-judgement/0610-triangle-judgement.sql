ed55876b8c217847979e5e7787d4b5c4a8e17065
GaYeon-Alice <146514128+GaYeon-Alice@users.noreply.github.com>
2025-02-11 23:06:59 +0900

    Time: 386 ms (32.89%), Space: 0B (100%) - LeetHub

--- LeetCode/Easy/0610-triangle-judgement/0610-triangle-judgement.sql
+++ LeetCode/Easy/0610-triangle-judgement/0610-triangle-judgement.sql
@@ -1,5 +1,7 @@
 -- Write your PostgreSQL query statement below
-SELECT *
+SELECT x
+     , y
+     , z
      , CASE
             WHEN x + y > z AND x + z > y AND y + z > x THEN 'Yes'
             ELSE 'No'
