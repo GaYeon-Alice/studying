419ff63fd88237aeab2dd7260c6f3a42432304ba
GaYeon-Alice <146514128+GaYeon-Alice@users.noreply.github.com>
2025-02-12 18:43:16 +0900

    Time: 350 ms (49%), Space: 0B (100%) - LeetHub

--- LeetCode/Medium/1321-restaurant-growth/1321-restaurant-growth.sql
+++ LeetCode/Medium/1321-restaurant-growth/1321-restaurant-growth.sql
@@ -1,21 +1,13 @@
-# Write your MySQL query statement below
-WITH DailyAmount AS (  -- 일별 판매금액 합계
+-- Write your PostgreSQL query statement below
+WITH DailySales AS (
     SELECT visited_on
          , SUM(amount) AS amount
     FROM Customer
     GROUP BY visited_on
 )
 SELECT visited_on
-     , SUM(amount) OVER 
-        (ORDER BY visited_on
-         ROWS 6 PRECEDING) AS amount  -- 현재 날짜까지 7일 간의 판매금액의 합계
-     , ROUND(
-        SUM(amount) OVER (
-            ORDER BY visited_on
-            ROWS 6 PRECEDING
-        ) / 7
-       , 2) AS average_amount  -- 현재 날짜까지 7일 간의 판매금액의 평균
-FROM DailyAmount
-GROUP BY visited_on
+     , SUM(amount) OVER (ORDER BY visited_on ROWS 6 PRECEDING) AS amount
+     , ROUND(AVG(amount) OVER (ORDER BY visited_on ROWS 6 PRECEDING), 2) AS average_amount
+FROM DailySales
 ORDER BY visited_on
-LIMIT 6, 100;  -- 6번째 행까지 무시하고 7번째 행부터 100개의 행을 출력 (7 ~ 106)
+OFFSET 6
