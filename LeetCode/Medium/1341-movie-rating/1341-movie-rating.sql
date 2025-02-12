c5629243425d945338bbdc2fce41aa099fe0aed0
GaYeon-Alice <146514128+GaYeon-Alice@users.noreply.github.com>
2025-02-12 18:28:15 +0900

    Time: 466 ms (55.99%), Space: 0B (100%) - LeetHub

--- LeetCode/Medium/1341-movie-rating/1341-movie-rating.sql
+++ LeetCode/Medium/1341-movie-rating/1341-movie-rating.sql
@@ -1,21 +1,19 @@
-# Write your MySQL query statement below
+-- Write your PostgreSQL query statement below
 (
     SELECT u.name AS results
     FROM Users AS u
-    JOIN MovieRating AS mr ON u.user_id = mr.user_id
+         JOIN MovieRating AS r USING (user_id)
     GROUP BY u.name
-    ORDER BY COUNT(*) DESC
-           , u.name
+    ORDER BY COUNT(DISTINCT r.movie_id) DESC, u.name
     LIMIT 1
 )
 UNION ALL
 (
     SELECT m.title AS results
     FROM Movies AS m
-    JOIN MovieRating AS mr ON m.movie_id = mr.movie_id
-    WHERE DATE_FORMAT(mr.created_at, '%Y-%m') = '2020-02'
+         JOIN MovieRating AS r USING (movie_id)
+    WHERE TO_CHAR(created_at, 'YYYY-MM') = '2020-02'
     GROUP BY m.title
-    ORDER BY AVG(mr.rating) DESC
-           , m.title
+    ORDER BY AVG(rating) DESC, m.title
     LIMIT 1
-);
+)
