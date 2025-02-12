afa89608db7f8aa3939f397c42c9c2d4e3975f05
GaYeon-Alice <146514128+GaYeon-Alice@users.noreply.github.com>
2025-02-12 18:44:38 +0900

    Time: 516 ms (13.25%), Space: 0B (100%) - LeetHub

--- LeetCode/Medium/1321-restaurant-growth/1321-restaurant-growth.sql
+++ LeetCode/Medium/1321-restaurant-growth/1321-restaurant-growth.sql
@@ -1,13 +1,13 @@
 -- Write your PostgreSQL query statement below
 WITH DailySales AS (
     SELECT visited_on
-         , SUM(amount) AS amount
+         , SUM(amount) AS total
     FROM Customer
     GROUP BY visited_on
 )
 SELECT visited_on
-     , SUM(amount) OVER (ORDER BY visited_on ROWS 6 PRECEDING) AS amount
-     , ROUND(AVG(amount) OVER (ORDER BY visited_on ROWS 6 PRECEDING), 2) AS average_amount
+     , SUM(total) OVER (ORDER BY visited_on ROWS 6 PRECEDING) AS amount
+     , ROUND(AVG(total) OVER (ORDER BY visited_on ROWS 6 PRECEDING), 2) AS average_amount
 FROM DailySales
 ORDER BY visited_on
 OFFSET 6
