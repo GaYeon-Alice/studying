e3455accd185b52768ff309bb1d41a6ce7024a00
GaYeon-Alice <146514128+GaYeon-Alice@users.noreply.github.com>
2025-02-12 18:10:32 +0900

    Time: 296 ms (72.81%), Space: 0B (100%) - LeetHub

--- LeetCode/Medium/0626-exchange-seats/0626-exchange-seats.sql
+++ LeetCode/Medium/0626-exchange-seats/0626-exchange-seats.sql
@@ -1,8 +1,8 @@
-# Write your MySQL query statement below
+-- Write your PostgreSQL query statement below
 SELECT id
      , CASE
-           WHEN id%2 = 0 THEN LAG(student) OVER(ORDER BY id)  -- 짝수 행이면 다음 값
-           ELSE IFNULL(LEAD(student) OVER(ORDER BY id), student)  -- 홀수 행이면 이전 값 (단, 마지막 행이 홀수 행이면 해당 값은 그대로 출력)
+            WHEN id % 2 = 0 THEN LAG(student) OVER (ORDER BY id)
+            ELSE COALESCE(LEAD(student) OVER (ORDER BY id), student)
        END AS student
 FROM Seat
-ORDER BY id;
+ORDER BY id
