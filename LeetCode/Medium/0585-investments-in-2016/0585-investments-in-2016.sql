b37e88742911ba9613c0475cb6735a166453027c
GaYeon-Alice <146514128+GaYeon-Alice@users.noreply.github.com>
2025-02-12 18:59:22 +0900

    Time: 413 ms (77.87%), Space: 0B (100%) - LeetHub

--- LeetCode/Medium/0585-investments-in-2016/0585-investments-in-2016.sql
+++ LeetCode/Medium/0585-investments-in-2016/0585-investments-in-2016.sql
@@ -1,17 +1,15 @@
-# Write your MySQL query statement below
-SELECT ROUND(SUM(tiv_2016)) AS tiv_2016
+-- Write your PostgreSQL query statement below
+SELECT ROUND(SUM(tiv_2016)::decimal, 2) AS tiv_2016
 FROM Insurance
 WHERE tiv_2015 IN (
     SELECT tiv_2015
     FROM Insurance
     GROUP BY tiv_2015
-    HAVING COUNT(*) > 1  -- tiv_2015가 중복되는 경우
-)
-  AND (lat, lon) IN (
+    HAVING COUNT(*) > 1
+) AND (lat, lon) IN (
     SELECT lat
          , lon
     FROM Insurance
-    GROUP BY lat
-           , lon
-    HAVING COUNT(*) = 1  -- (lat, lon)이 중복되지 않는 경우
-);
+    GROUP BY lat, lon
+    HAVING COUNT(*) = 1
+)
