0b65360d9434ee857a3464f4c855e92d07f71d53
GaYeon-Alice <146514128+GaYeon-Alice@users.noreply.github.com>
2025-02-13 11:31:17 +0900

    Time: 271 ms (89.49%), Space: 0B (100%) - LeetHub

--- LeetCode/Easy/0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
+++ LeetCode/Easy/0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
@@ -1,6 +1,5 @@
-# Write your MySQL query statement below
-DELETE p1
-FROM Person AS p1
-JOIN Person AS p2
-ON p1.email = p2.email
-AND p1.id > p2.id;
+-- Write your PostgreSQL query statement below
+DELETE FROM Person AS p1
+USING Person AS p2
+WHERE p1.email = p2.email
+AND p1.id > p2.id
