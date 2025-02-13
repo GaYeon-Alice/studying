866f1d94c09133c65c66e44b2853cf3beb702f42
GaYeon-Alice <146514128+GaYeon-Alice@users.noreply.github.com>
2025-02-13 11:30:13 +0900

    Time: 1686 ms (25.96%), Space: 0B (100%) - LeetHub

--- LeetCode/Easy/0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
+++ LeetCode/Easy/0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
@@ -1,5 +1,6 @@
 # Write your MySQL query statement below
 DELETE p1
-FROM Person AS p1, Person AS p2
-WHERE p1.email = p2.email
-  AND p1.id > p2.id;  -- 이메일이 중복되는 경우, id 값이 작은 행이 남아 있어야 하므로 id 값이 더 큰 행을 삭제
+FROM Person AS p1
+JOIN Person AS p2
+ON p1.email = p2.email
+AND p1.id > p2.id;
