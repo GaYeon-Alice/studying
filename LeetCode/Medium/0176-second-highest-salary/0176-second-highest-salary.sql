462531e3d2764d97a829c2715451387cecd79880
GaYeon-Alice <146514128+GaYeon-Alice@users.noreply.github.com>
2025-02-13 11:47:32 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Medium/0176-second-highest-salary/0176-second-highest-salary.sql
+++ LeetCode/Medium/0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,4 +1,8 @@
 -- Write your PostgreSQL query statement below
-SELECT MAX(salary) AS SecondHighestSalary
-FROM Employee
-WHERE salary < (SELECT MAX(salary) FROM Employee)
+SELECT MAX(salary) AS "SecondHighestSalary"
+FROM (
+    SELECT salary
+         , DENSE_RANK() OVER (ORDER BY salary) AS dr
+    FROM Employee
+) AS rk
+WHERE rk.dr = 2
