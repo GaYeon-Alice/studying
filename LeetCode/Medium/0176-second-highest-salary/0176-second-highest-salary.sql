3993a0543d14f7847e86793a2e7e2abe2e38c070
GaYeon-Alice <146514128+GaYeon-Alice@users.noreply.github.com>
2025-02-13 11:35:26 +0900

    Time: 289 ms (82.32%), Space: 0B (100%) - LeetHub

--- LeetCode/Medium/0176-second-highest-salary/0176-second-highest-salary.sql
+++ LeetCode/Medium/0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,4 +1,4 @@
-# Write your MySQL query statement below
-SELECT MAX(salary) AS SecondHighestSalary  -- 최댓값이 아닌 값들 중에서 최댓값 조회
+-- Write your PostgreSQL query statement below
+SELECT MAX(salary) AS SecondHighestSalary
 FROM Employee
-WHERE salary < (SELECT MAX(salary) FROM Employee);
+WHERE salary < (SELECT MAX(salary) FROM Employee)
