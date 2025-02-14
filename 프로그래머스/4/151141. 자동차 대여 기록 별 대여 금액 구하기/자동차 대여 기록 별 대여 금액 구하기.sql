d99fdb9b1baf14b857129bbdfeeee6fc61a5ac2b
GaYeon-Alice <146514128+GaYeon-Alice@users.noreply.github.com>
2025-02-14 15:49:53 +0900

    [level 4] Title: 자동차 대여 기록 별 대여 금액 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/151141. 자동차 대여 기록 별 대여 금액 구하기/자동차 대여 기록 별 대여 금액 구하기.sql
+++ 프로그래머스/4/151141. 자동차 대여 기록 별 대여 금액 구하기/자동차 대여 기록 별 대여 금액 구하기.sql
@@ -1,21 +1,25 @@
 -- 코드를 입력하세요
-WITH RENTAL_INFO AS (
-    SELECT h.history_id
+WITH rental_info AS (
+    SELECT r.history_id
          , c.daily_fee
-         , c.car_type
-         , DATEDIFF(h.end_date, h.start_date) + 1 AS rental_days
-    FROM CAR_RENTAL_COMPANY_CAR AS c
-    JOIN CAR_RENTAL_COMPANY_RENTAL_HISTORY AS h ON c.car_id = h.car_id
+         , DATEDIFF(r.end_date, r.start_date) + 1 AS rental_days
+    FROM car_rental_company_rental_history AS r
+    JOIN car_rental_company_car AS c
+      ON r.car_id = c.car_id
     WHERE c.car_type = '트럭'
+), discount_info AS (
+    SELECT duration_type
+         , discount_rate
+    FROM car_rental_company_discount_plan
+    WHERE car_type = '트럭'
 )
-SELECT r.history_id
-     , FLOOR(r.daily_fee * (1 - 0.01 * COALESCE(d.discount_rate, 0)) * r.rental_days) AS fee
-FROM RENTAL_INFO AS r
-LEFT JOIN CAR_RENTAL_COMPANY_DISCOUNT_PLAN AS d
-    ON r.car_type = d.car_type
-    AND (
-        (d.duration_type = '7일 이상' AND r.rental_days BETWEEN 7 AND 29)
-        OR (d.duration_type = '30일 이상' AND r.rental_days BETWEEN 30 AND 89)
-        OR (d.duration_type = '90일 이상' AND r.rental_days >= 90)
-    ) 
-ORDER BY fee DESC, r.history_id DESC;
+SELECT i.history_id,
+       FLOOR(i.daily_fee * i.rental_days * (1 - 0.01 * COALESCE(d.discount_rate, 0))) AS fee
+FROM rental_info AS i
+LEFT JOIN discount_info AS d
+  ON (
+         (d.duration_type = '7일 이상' AND i.rental_days BETWEEN 7 AND 29)
+      OR (d.duration_type = '30일 이상' AND i.rental_days BETWEEN 30 AND 89)
+      OR (d.duration_type = '90일 이상' AND i.rental_days >= 90)
+)
+ORDER BY fee DESC, i.history_id DESC;
