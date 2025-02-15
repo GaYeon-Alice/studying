7083d395ae59d333d11b1b76a592b420bae6b2bc
GaYeon-Alice <146514128+GaYeon-Alice@users.noreply.github.com>
2025-02-15 19:44:59 +0900

    [level 4] Title: 특정 기간동안 대여 가능한 자동차들의 대여비용 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/157339. 특정 기간동안 대여 가능한 자동차들의 대여비용 구하기/특정 기간동안 대여 가능한 자동차들의 대여비용 구하기.sql
+++ 프로그래머스/4/157339. 특정 기간동안 대여 가능한 자동차들의 대여비용 구하기/특정 기간동안 대여 가능한 자동차들의 대여비용 구하기.sql
@@ -1,23 +1,28 @@
 -- 코드를 입력하세요
-SELECT *
-FROM (
-    SELECT C.CAR_ID
-         , C.CAR_TYPE
-         , ROUND(C.DAILY_FEE * (1 - 0.01 * D.DISCOUNT_RATE) * 30, 0) AS FEE  -- 대여 금액 계산
-    FROM CAR_RENTAL_COMPANY_CAR AS C
-    JOIN CAR_RENTAL_COMPANY_DISCOUNT_PLAN AS D ON C.CAR_TYPE = D.CAR_TYPE
-    WHERE C.CAR_TYPE IN ('세단', 'SUV')  -- 자동차 종류 필터
-      AND D.DURATION_TYPE = '30일 이상'  -- 대여 기간 필터
-      AND C.CAR_ID NOT IN (
-          -- 2022년 11월 1일부터 11월 30일 사이에 대여가 불가능한 자동차 ID
-          SELECT CAR_ID
-          FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY
-          WHERE START_DATE <= '2022-11-30'
-            AND END_DATE >= '2022-11-01'
-          )
-) AS TMP
-WHERE FEE >= 500000  -- 대여 금액 필터
-  AND FEE < 2000000
-ORDER BY FEE DESC  -- 대여 금액 내림차순
-       , CAR_TYPE  -- 자동차 종류 오름차순
-       , CAR_ID DESC;  -- 자동차 ID 내림차순
+WITH available_cars AS (
+    SELECT DISTINCT car_id
+    FROM car_rental_company_rental_history
+    WHERE car_id NOT IN (
+        SELECT car_id
+        FROM car_rental_company_rental_history
+        WHERE start_date <= '2022-11-30'
+          AND end_date >= '2022-11-01'
+    )
+), rental_fee AS (
+    SELECT a.car_id
+         , c.car_type
+         , FLOOR(c.daily_fee * 30 * (1 - 0.01 * discount_rate)) AS fee
+    FROM available_cars AS a
+    JOIN car_rental_company_car AS c
+      ON a.car_id = c.car_id
+    JOIN car_rental_company_discount_plan AS d
+      ON c.car_type = d.car_type
+    WHERE c.car_type IN ('세단', 'SUV')
+      AND d.duration_type = '30일 이상'
+)
+SELECT car_id
+     , car_type
+     , fee
+FROM rental_fee
+WHERE fee >= 500000 AND fee < 2000000
+ORDER BY fee DESC, car_type, car_id DESC;
