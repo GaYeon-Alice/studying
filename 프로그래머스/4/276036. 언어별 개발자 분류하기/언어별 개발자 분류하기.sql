a2e0d3f867fcf21458b528c2eb4f23070095647c
GaYeon-Alice <146514128+GaYeon-Alice@users.noreply.github.com>
2025-02-18 17:54:02 +0900

    [level 4] Title: 언어별 개발자 분류하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/276036. 언어별 개발자 분류하기/언어별 개발자 분류하기.sql
+++ 프로그래머스/4/276036. 언어별 개발자 분류하기/언어별 개발자 분류하기.sql
@@ -1,19 +1,15 @@
 -- 코드를 작성해주세요
 SELECT CASE
-           -- A: Front End 스킬과 Python 스킬을 함께 가지고 있는 개발자
-           WHEN MAX(CASE WHEN S.CATEGORY = 'Front End' THEN 1 ELSE 0 END) > 0
-            AND MAX(CASE WHEN S.NAME = 'Python' THEN 1 ELSE 0 END) > 0 THEN 'A'
-           -- B: C# 스킬을 가진 개발자
-           WHEN MAX(CASE WHEN S.NAME = 'C#' THEN 1 ELSE 0 END) > 0 THEN 'B'
-           -- C: 그 외의 Front End 개발자
-           WHEN MAX(CASE WHEN S.CATEGORY = 'Front End' THEN 1 ELSE 0 END) > 0 THEN 'C'
-       END AS GRADE
-       , D.ID
-       , D.EMAIL
-FROM DEVELOPERS AS D
-JOIN SKILLCODES AS S ON D.SKILL_CODE & S.CODE  -- SKILL CODE와 CODE를 비트 단위로 연산하여 개발자의 스킬 조회
-GROUP BY D.ID
-       , D.EMAIL
-HAVING GRADE IS NOT NULL
-ORDER BY GRADE
-       , D.ID;
+            WHEN SUM(CASE WHEN s.category = 'Front End' THEN 1 ELSE 0 END) > 0 
+             AND SUM(CASE WHEN s.name = 'Python' THEN 1 ELSE 0 END) > 0 THEN 'A'
+            WHEN SUM(CASE WHEN s.name = 'C#' THEN 1 ELSE 0 END) > 0 THEN 'B'
+            WHEN SUM(CASE WHEN s.category = 'Front End' THEN 1 ELSE 0 END) > 0 THEN 'C'
+       END AS grade
+     , d.id
+     , d.email
+FROM developers AS d
+JOIN skillcodes AS s
+  ON d.skill_code & s.code
+GROUP BY d.id, d.email
+HAVING grade IS NOT NULL
+ORDER BY grade, d.id;
