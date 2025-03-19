9c2e3b548d20aaf7f9fc3f2c384ba6650625f94f
GaYeon-Alice <146514128+GaYeon-Alice@users.noreply.github.com>
2025-03-19 22:58:47 +0900

    [level 2] Title: 전화번호 목록, Time: 95.95 ms, Memory: 29.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -0,0 +1,14 @@
+def solution(phone_book):
+    
+    # 전화번호를 오름차순으로 정렬
+    phone_book.sort()
+    
+    # 인접한 두 전화번호만 비교하면 됨
+    for i in range(len(phone_book) - 1):
+    
+        # 현재 번호가 다음 번호의 접두어인지 확인
+        if phone_book[i + 1].startswith(phone_book[i]):
+            return False
+    
+    # 접두어 관계가 없으면 True 반환
+    return True
