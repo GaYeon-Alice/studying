c6046dff157c833e0951edfe897afc46b7fd9245
GaYeon-Alice <146514128+GaYeon-Alice@users.noreply.github.com>
2025-03-20 17:18:42 +0900

    Time: 4 ms (30.64%), Space: 17.8 MB (47.42%) - LeetHub

--- LeetCode/Easy/0819-most-common-word/0819-most-common-word.py
+++ LeetCode/Easy/0819-most-common-word/0819-most-common-word.py
@@ -1,6 +1,17 @@
+import re
+from collections import Counter
+
 class Solution:
     def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
-        paragraph = re.sub('\W', ' ', paragraph).lower()
-        word_list = [word for word in paragraph.split() if word not in banned]
+
+        # 단어 문자(a-z, A-Z, 0-9, _)가 아닌 모든 문자를 공백으로 치환 후 소문자로 변경
+        cleaned_text = re.sub(r'\W', ' ', paragraph).lower()
+
+        # 단어 목록 추출
+        words = cleaned_text.split()
         
-        return Counter(word_list).most_common(1)[0][0]
+        # 금지된 단어를 제외한 단어 목록 추출
+        filtered_words = [word for word in words if word not in banned]
+
+        # 가장 자주 등장하는 단어 출력
+        return Counter(filtered_words).most_common(1)[0][0]
