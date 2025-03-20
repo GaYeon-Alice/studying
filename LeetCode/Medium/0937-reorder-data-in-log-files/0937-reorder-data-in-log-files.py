c962840f87ff5c3b4735f6026d809e8387fe6b2d
GaYeon-Alice <146514128+GaYeon-Alice@users.noreply.github.com>
2025-03-20 16:13:43 +0900

    Time: 0 ms (100%), Space: 17.9 MB (70.1%) - LeetHub

--- LeetCode/Medium/0937-reorder-data-in-log-files/0937-reorder-data-in-log-files.py
+++ LeetCode/Medium/0937-reorder-data-in-log-files/0937-reorder-data-in-log-files.py
@@ -3,7 +3,11 @@ class Solution:
         let, dig = [], []
 
         for log in logs:
-            let.append(log) if log.split()[1].isalpha() else dig.append(log)
+            if log.split()[1].isalpha():
+                let.append(log)
+            else:
+                dig.append(log)
+        
         let.sort(key=lambda x: (x.split()[1:], x.split()[0]))
-            
+
         return let + dig
