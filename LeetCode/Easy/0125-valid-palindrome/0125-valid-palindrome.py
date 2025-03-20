44575e791816f890313adf535b6c6c315f0ca9b2
GaYeon-Alice <146514128+GaYeon-Alice@users.noreply.github.com>
2025-03-20 13:34:51 +0900

    Time: 5 ms (87.42%), Space: 19.3 MB (24.25%) - LeetHub

--- LeetCode/Easy/0125-valid-palindrome/0125-valid-palindrome.py
+++ LeetCode/Easy/0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,5 +1,13 @@
+import re
+
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        s = re.sub('[^a-zA-Z0-9]', '', s).lower()
-        reversed_s = ''.join(reversed(s))
-        return s == reversed_s
+
+        # 영문 대문자를 모두 소문자로 변경
+        s = s.lower()
+
+        # 영문 소문자와 숫자 이외의 모든 문자 제거(공백도 제거)
+        s = re.sub('[^a-z0-9]', '', s)
+
+        # 팰린드롬이면 true, 아니면 false 반환
+        return s == s[::-1]
