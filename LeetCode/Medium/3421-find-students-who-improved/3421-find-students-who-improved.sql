43f9697e42519699146a6e22a5726fc0c7f4317e
GaYeon-Alice <146514128+GaYeon-Alice@users.noreply.github.com>
2025-05-03 21:39:50 +0900

    Time: 198 ms (63.36%), Space: 0B (100%) - LeetHub

--- LeetCode/Medium/3421-find-students-who-improved/3421-find-students-who-improved.sql
+++ LeetCode/Medium/3421-find-students-who-improved/3421-find-students-who-improved.sql
@@ -1,26 +1,25 @@
-# Write your MySQL query statement below
-WITH compare_score AS (
+-- Write your PostgreSQL query statement below
+WITH scorehistory AS (
     SELECT student_id
          , subject
          , score
          , exam_date
-         , ROW_NUMBER() OVER (PARTITION BY student_id, subject ORDER BY exam_date) AS rn1  -- 첫 시험
-         , ROW_NUMBER() OVER (PARTITION BY student_id, subject ORDER BY exam_date DESC) AS rn2  -- 마지막 시험
-    FROM scores AS s
-    WHERE EXISTS (
-        SELECT 1
-        FROM scores AS s2
-        WHERE s2.student_id = s.student_id
-          AND s2.subject = s.subject
-        GROUP BY s2.student_id, s2.subject
-        HAVING COUNT(DISTINCT s2.exam_date) >= 2  -- 동일한 과목 시험을 두 번 이상 본 학생만 추출
+         , ROW_NUMBER() OVER (PARTITION BY student_id, subject ORDER BY exam_date) AS first_exam
+         , ROW_NUMBER() OVER (PARTITION BY student_id, subject ORDER BY exam_date DESC) AS latest_exam
+    FROM scores
+    WHERE (student_id, subject) IN (
+        SELECT student_id
+             , subject
+        FROM scores
+        GROUP BY student_id, subject
+        HAVING COUNT(DISTINCT exam_date) >= 2
     )
 )
 SELECT student_id
      , subject
-     , MAX(CASE WHEN rn1 = 1 THEN score END) AS first_score
-     , MAX(CASE WHEN rn2 = 1 THEN score END) AS latest_score
-FROM compare_score
+     , MAX(score) FILTER (WHERE first_exam = 1) AS first_score
+     , MAX(score) FILTER (WHERE latest_exam = 1) AS latest_score
+FROM scorehistory
 GROUP BY student_id, subject
-HAVING first_score < latest_score  -- 점수가 오른 학생
+HAVING MAX(score) FILTER (WHERE first_exam = 1) < MAX(score) FILTER (WHERE latest_exam = 1)
 ORDER BY student_id, subject;
