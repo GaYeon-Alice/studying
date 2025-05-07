2c4c5241efd5b2eb5100cf1f9435dbf55af1aab1
GaYeon-Alice <146514128+GaYeon-Alice@users.noreply.github.com>
2025-05-07 22:36:47 +0900

    Time: 379 ms (40.22%), Space: 0B (100%) - LeetHub

--- LeetCode/Easy/3465-find-products-with-valid-serial-numbers/3465-find-products-with-valid-serial-numbers.sql
+++ LeetCode/Easy/3465-find-products-with-valid-serial-numbers/3465-find-products-with-valid-serial-numbers.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
 SELECT *
 FROM products
-WHERE description REGEXP 'SN[0-9]{4}-[0-9]{4}( |$)'
+WHERE description REGEXP '(^|[[:space:]])SN[0-9]{4}-[0-9]{4}( |$)'
 ORDER BY product_id;
