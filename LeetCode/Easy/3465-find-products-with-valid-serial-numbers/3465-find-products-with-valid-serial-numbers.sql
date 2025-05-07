8b1ae916db714a49880963e366512f7eefd11dcd
GaYeon-Alice <146514128+GaYeon-Alice@users.noreply.github.com>
2025-05-07 22:35:57 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Easy/3465-find-products-with-valid-serial-numbers/3465-find-products-with-valid-serial-numbers.sql
+++ LeetCode/Easy/3465-find-products-with-valid-serial-numbers/3465-find-products-with-valid-serial-numbers.sql
@@ -1,6 +1,5 @@
 # Write your MySQL query statement below
 SELECT *
 FROM products
-WHERE description LIKE '%SN____-____ %'  -- 문장 중간에 serial number가 있는 경우
-   OR description LIKE '%SN____-____'   -- 문장 끝에 serial number가 있는 경우
+WHERE description REGEXP 'SN[0-9]{4}-[0-9]{4}( |$)'
 ORDER BY product_id;
