f792b63269ad503b73cd9822d0376cda8475ba48
GaYeon-Alice <146514128+GaYeon-Alice@users.noreply.github.com>
2025-05-13 12:34:59 +0900

    Time: 339 ms (35.07%), Space: 0B (100%) - LeetHub

--- LeetCode/Medium/3521-find-product-recommendation-pairs/3521-find-product-recommendation-pairs.sql
+++ LeetCode/Medium/3521-find-product-recommendation-pairs/3521-find-product-recommendation-pairs.sql
@@ -1,21 +1,24 @@
-# Write your MySQL query statement below
-WITH purchaseinfo AS (
+-- Write your PostgreSQL query statement below
+-- 사용자별 구매 상품과 해당 상품의 카테고리 정보 결합
+WITH PurchaseInfo AS (
     SELECT p.user_id
          , p.product_id
          , i.category
-    FROM productpurchases AS p
-    JOIN productinfo AS i
+    FROM ProductPurchases AS p
+    JOIN ProductInfo AS i
         ON p.product_id = i.product_id
 )
+
+-- 동일 사용자가 함께 구매한 서로 다른 두 상품 쌍 추출
 SELECT pi1.product_id AS product1_id
      , pi2.product_id AS product2_id
      , pi1.category AS product1_category
      , pi2.category AS product2_category
-     , COUNT(*) AS customer_count
-FROM purchaseinfo AS pi1
-JOIN purchaseinfo AS pi2
+     , COUNT(*) AS customer_count  -- 함께 구매한 사용자 수
+FROM PurchaseInfo AS pi1
+JOIN PurchaseInfo AS pi2
     ON pi1.product_id < pi2.product_id
     AND pi1.user_id = pi2.user_id
-GROUP BY product1_id, product2_id
-HAVING customer_count >= 3
+GROUP BY product1_id, product2_id, product1_category, product2_category
+HAVING COUNT(*) >= 3  -- 최소 3명의 사용자가 함께 구매한 상품 쌍만 추출
 ORDER BY customer_count DESC, product1_id, product2_id;
