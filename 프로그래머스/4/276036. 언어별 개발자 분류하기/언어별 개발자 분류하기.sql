cc323eb51004581c82c6ac35f5f15d8f9ce79164
GaYeon-Alice <146514128+GaYeon-Alice@users.noreply.github.com>
2025-06-11 16:52:35 +0900

    [level 4] Title: 언어별 개발자 분류하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/276036. 언어별 개발자 분류하기/언어별 개발자 분류하기.sql
+++ 프로그래머스/4/276036. 언어별 개발자 분류하기/언어별 개발자 분류하기.sql
@@ -1,15 +1,15 @@
 -- 코드를 작성해주세요
 SELECT CASE
-            WHEN SUM(CASE WHEN s.category = 'Front End' THEN 1 ELSE 0 END) > 0 
-             AND SUM(CASE WHEN s.name = 'Python' THEN 1 ELSE 0 END) > 0 THEN 'A'
-            WHEN SUM(CASE WHEN s.name = 'C#' THEN 1 ELSE 0 END) > 0 THEN 'B'
-            WHEN SUM(CASE WHEN s.category = 'Front End' THEN 1 ELSE 0 END) > 0 THEN 'C'
+         WHEN SUM(CASE WHEN s.category = 'Front End' THEN 1 ELSE 0 END) > 0 
+          AND SUM(CASE WHEN s.name = 'Python' THEN 1 ELSE 0 END) > 0 THEN 'A'
+         WHEN SUM(CASE WHEN s.name = 'C#' THEN 1 ELSE 0 END) > 0 THEN 'B'
+         WHEN SUM(CASE WHEN s.category = 'Front End' THEN 1 ELSE 0 END) > 0 THEN 'C'
        END AS grade
      , d.id
      , d.email
 FROM developers AS d
 JOIN skillcodes AS s
   ON d.skill_code & s.code
-GROUP BY d.id, d.email
+GROUP BY id, email
 HAVING grade IS NOT NULL
-ORDER BY grade, d.id;
+ORDER BY grade, id;
